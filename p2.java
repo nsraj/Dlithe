0d9dce06f3e514cd3ef0d0126a0ca53c4d9ba161
nsraj <96631397+nsraj@users.noreply.github.com>
2022-12-21 13:56:53 +0530

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,15 @@
+//Program for supermarket
+package myPackage;
+
+public class SuperMarket {
+
+	public static void main(String[] args) {
+		String str = new java.util.Scanner(System.in).next();
+		long res = 1;
+		int len = str.length();
+		for(int i = 0; i < len; i++)
+			res *= Character.getNumericValue(str.charAt(i));
+		System.out.println(res);
+	}
+
+}
